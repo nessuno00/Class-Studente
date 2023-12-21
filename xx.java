783c91f197e32c0d75b1a8191d25be52814018f1
nessuno00 <alect1@live.it>
2023-12-21 19:22:44 +0100

    commit on my branch

---
+++ xx.java
@@ -0,0 +1,35 @@
+
+/**
+ * Aggiungi qui una descrizione della classe xx
+ * 
+ * @author (il tuo nome) 
+ * @version (un numero di versione o una data)
+ */
+public class xx
+{
+    // variabili d'istanza - sostituisci l'esempio che segue con il tuo
+    private int x;
+
+    private int Y ;git bra
+
+    /**
+     * Costruttore degli oggetti di classe  xx
+     */
+    public xx()
+    {
+        // inizializza le variabili d'istanza
+        x = 0;
+    }
+
+    /**
+     * Un esempio di metodo - aggiungi i tuoi commenti
+     * 
+     * @param  y   un parametro d'esempio per un metodo
+     * @return     la somma di x e y
+     */
+    public int sampleMethod(int y)
+    {
+        // metti qui il tuo codice
+        return x + y;
+    }
+}
